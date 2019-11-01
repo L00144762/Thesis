3639cd279ff396555636e99304227f1b830849d4
L00144762 <L00144762@student.lyit.ie>
2019-11-01 14:52:18 +0000

    commit test 2

--- img_processing/sampling.py
+++ img_processing/sampling.py
@@ -3,4 +3,10 @@
 #Initial image processing: sampling the Animals-10 dataset
 # URL: https://www.kaggle.com/alessiocorrado99/animals10
 
-#import all the packages for this package
+#import all the packages for this Py_package
+
+##import images from animals
+
+# convert to grayscale
+# resize
+# make sure there's no people in them
