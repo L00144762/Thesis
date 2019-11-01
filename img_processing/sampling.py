eb5c272f0e2b9ab0bfb937db232cf958a3e056aa
L00144762 <L00144762@student.lyit.ie>
2019-11-01 13:29:09 +0000

    initial

---
+++ img_processing/sampling.py
@@ -0,0 +1,6 @@
+# Richard Curran
+# L00144762
+#Initial image processing: sampling the Animals-10 dataset
+# URL: https://www.kaggle.com/alessiocorrado99/animals10
+
+#import all the packages for this package
